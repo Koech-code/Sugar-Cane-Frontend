c604f26425995ad9ebeb34f5c3485a31b8ad9ec7
Koech-code <nixonkipkorir01@gmail.com>
2023-04-04 17:18:34 +0300

    modified homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,106 +12,56 @@ const Home = () => {
     navigate("/registration");
   };
 
-  const farmers =
-    "https://sentientmedia.org/wp-content/uploads/2022/03/james-baltz-yihX4Rq-JsI-unsplash.jpg";
-
-  const footerbg =
-    "https://www.ippmedia.com/sites/default/files/articles/2022/08/03/sugarcane%20farming.jpg";
   return (
-    <div className=" sm:w-full sm:h-full md:w-full md:h-full lg:w-full lg:h-full sm:text-center">
+    <div className="sm:w-full sm:h-full md:w-full md:h-full lg:w-full lg:h-full sm:text-center">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127648.30586518982!2d36.943234298093174!3d-1.153662538208368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f38a78cb4e50d%3A0xdaa7fad9c2cbf0fb!2sRuiru!5e0!3m2!1sen!2ske!4v1680545184543!5m2!1sen!2ske"
         width="1500"
-        height="450"
+        height="650"
         style={{ border: "0" }}
         allowfullscreen=""
         loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"
+        className="absolute"
       ></iframe>
-      <div className="relative h-64 ">
-        <img
-          className="absolute top-0 left-0 w-full h-full object-cover sm:object-fill "
-          src={farmers}
-          alt="background"
-        />
-
-        {/* <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-0"></div> */}
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <h1 className="text-4xl md:text-5xl font-bold text-white text-center mt-8 md:mt-16 mb-8 md:mb-12">
-            PERFORMANCE AT ELITE LEVEL
-          </h1>
-
+      <form className="w-96">
+        <div className="relative">
+          <input
+            type="text"
+            // value={query}
+            // onChange = {handleInputChange}
+            placeholder="Search"
+            className=" bg-white text-gray-900 rounded-full focus:border-none py-2 pr-4 pl-10 block w-full leading-5 border border-gray-300 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+          />
+          <div className="absolute inset-y-0 left-0 pl-4 flex items-center">
+            <button type="submit" className="border-none">
+              <svg
+                className="h-5 w-5 text-green-400"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+              </svg>
+            </button>
+          </div>
           <button
-            onClick={navigateRegistrationPage}
-            className="px-8 py-4 bg-[#2BAE76]  text-white font-bold rounded hover:bg-green-500 hover:text-white"
+            type="reset"
+            className="absolute right-0 top-0 mt-3 mr-12 bg-green-600"
           >
-            GET STARTED
+            <svg
+              className="h-4 w-4 fill-current "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 0c-5.5 0-10 4.5-10 10s4.5 10 10 10 10-4.5 10-10-4.5-10-10-10zm5.3 14.7c-.4.4-1 .4-1.4 0l-3.9-3.9-3.9 3.9c-.4.4-1 .4-1.4 0s-.4-1 0-1.4l3.9-3.9-3.9-3.9c-.4-.4-.4-1 0-1.4s1-.4 1.4 0l3.9 3.9 3.9-3.9c.4-.4 1-.4 1.4 0s.4 1 0 1.4l-3.9 3.9 3.9 3.9c.4.4.4 1 0 1.4z" />
+            </svg>
           </button>
         </div>
-      </div>
-      <div className="bg-white rounded-lg shadow-md p-4 m-8">
-        <h2 className="text-lg font-bold mb-2 text-center text-blue-600">
-          SPORTS SCHOLARSHIPS
-        </h2>
-        <p className="text-gray-600 p-8">
-          Take your athletic and academic career to the next level We help
-          athletes on their journey in getting a sports scholarships to study in
-          the USA and advance their sports careers.
-        </p>
-      </div>
-
-      <div className="flex flex-wrap justify-center">
-        <div className="bg-white rounded-lg shadow-md p-4 lg:w-1/3">
-          <h2 className="text-lg font-bold mb-2 text-center text-blue-600">
-            1. Assessment
-          </h2>
-          <p className="text-gray-600 p-4 lg:p-8">
-            Our team consist of athletes and former athletes who evaluate your
-            submitted registration requirements and if successful move you to
-            the next step.
-          </p>
-        </div>
-        <div className="bg-white rounded-lg shadow-md p-4 lg:w-1/3">
-          <h2 className="text-lg font-bold mb-2 text-center text-blue-600">
-            2. Preparation
-          </h2>
-          <p className="text-gray-600 p-4 lg:p-8">
-            We then pair and prepare you for your prospective college by helping
-            you gather requirements and other relevant material for a successful
-            admission.
-          </p>
-        </div>
-        <div className="bg-white rounded-lg shadow-md p-4 lg:w-1/3">
-          <h2 className="text-lg font-bold mb-2 text-center text-blue-600">
-            3. Acceptance
-          </h2>
-          <p className="text-gray-600 p-4 lg:p-8">
-            Success! You begin a new and fulfilling journey in your academic and
-            athletic career in a top US college.
-          </p>
-        </div>
-      </div>
-
-      <div className="relative h-96">
-        <img
-          className="absolute top-0 left-0 w-full h-full object-cover sm:object-fill md:object-fill"
-          src={footerbg}
-          alt="background"
-        />
-        <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-0"></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <h1 className="text-4xl md:text-5xl font-bold text-white text-center mt-8 md:mt-16 mb-8 md:mb-12">
-            TRY THE FREE ASSESSMENT
-          </h1>
-          <button
-            onClick={navigateRegistrationPage}
-            className="px-8 py-4 bg-[#2BAE76]  text-white font-bold rounded hover:bg-green-500 hover:text-white"
-          >
-            NOW!
-          </button>
-        </div>
-      </div>
-      <Footer />
+      </form>
     </div>
   );
 };
